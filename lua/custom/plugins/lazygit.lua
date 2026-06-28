171eed44ef5f077958a3142529ef03fd28f477b9
hulk-higakijin <hm385.chejptks@gmail.com>
2026-06-28 12:21:49 +0900

    feat: lazygit

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,6 @@
+vim.pack.add {
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/kdheepak/lazygit.nvim',
+}
+
+vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', { desc = 'LazyGit' })
